ae67dcce29ce77a7ff8af09444bd60d97644e5b5
SanjayVuppala <132779398+SanjayVuppala@users.noreply.github.com>
2023-12-06 23:18:09 +0530

    Update config.php

--- banker/includes/config.php
+++ banker/includes/config.php
@@ -2,7 +2,7 @@
 //time zone
 date_default_timezone_set('Asia/Kolkata');
 //database connection
-$con=mysqli_connect("localhost","root","","blmsdb");
+$con=mysqli_connect("sanjay.mysql.database.azure.com","sanjay","sas1234*","sanjay112");
 if(mysqli_connect_errno()){
 echo "Connection Fail".mysqli_connect_error();
 }
